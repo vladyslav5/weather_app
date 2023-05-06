ba537d35ef21040c1ae6cebc95373038f9dbbda3
Vladyslav Morschch <vmorshch8@gmail.com>
2023-05-06 09:42:21 +0300

    initial commit

---
+++ src/components/Search/Search.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Search = () => {
+    return (
+        <div>
+            <input placeholder={"search for places"} className={"m-10 bg-gray-200"} />
+            <div>home</div>
+        </div>
+    );
+};
+
+export default Search;
---
+++ src/routes.tsx
@@ -0,0 +1,12 @@
+import {ReactNode} from "react";
+import {Route} from "react-router-dom";
+
+interface Route {
+    path: string
+    element: ReactNode
+}
+export const routes:Route[] = [
+    {
+        path:
+    }
+]
